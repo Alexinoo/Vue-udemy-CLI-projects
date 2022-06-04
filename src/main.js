89b662eaea03822abad92aded869e6afb54bb1b7
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 15:14:50 +0300

    Imported App.vue into main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 
-// Main entry point for JS
+/* Main entry point for JS
+
+-Importing vue - don't need to specify the path since vue is a package we have imported in package.json
+
+-When importing a custom component - We need to specify the path ./ or ../
+
+- Use {} for a named export
+
+-Or just App for a default export by using the name - could be anything , Hello/ShakeandBake but a good practice is to name it as the component object etc
+
+-Then we pass it into createApp() method
+
+*/
 
 import { createApp } from 'vue';
 
-createApp({}).mount('');
+import App from './App.vue'
+
+createApp(App).mount('#app');
