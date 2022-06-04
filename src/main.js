1d5998773c7e440915117cfe6d12f09b2e39a797
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 21:01:05 +0300

    Imported NewFriend and registered it as a component

--- src/main.js
+++ src/main.js
@@ -25,9 +25,13 @@ import App from './App.vue';
 
 import FriendContact from './Components/FriendContact.vue';
 
+import NewFriend from './Components/NewFriend.vue';
+
 const app = createApp(App)
 
 app.component('friend-contact', FriendContact )
+
+app.component('new-friend', NewFriend )
 
 app.mount('#app');
 
