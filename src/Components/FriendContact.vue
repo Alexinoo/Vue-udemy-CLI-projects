2df3e93ca76cdf461652fbd2cb0856748beb69a0
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 15:36:37 +0300

    Created Components directory; Added FriendContact.vue component with a tenplate and its own data

---
+++ src/Components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+    <li>
+        <h2> {{ friend.name }}</h2>
+        <button @click="toggleDetails"> Show Details</button>
+        <ul v-if="areDetailsVisible">
+            <li><strong>Phone : </strong>{{ friend.phone }}</li>
+            <li><strong>Email : </strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+
+    data(){
+
+        return {
+            areDetailsVisible : false ,
+            friend : {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '+254 4934 6734 783',
+                email: 'manuel@localhost.com'
+            }
+        }
+    },
+
+    methods : {
+        toggleDetails(){
+            this.areDetailsVisible = !this.areDetailsVisible
+        }
+    }
+
+    }
+</script>
