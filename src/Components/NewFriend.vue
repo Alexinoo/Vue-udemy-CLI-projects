8c93c4107d1c453ba7cae6c24f9f0d70ab118569
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 21:00:14 +0300

    Added a component with a form for addin a new friend ;No props ;emitted add-contact custom event to App.vue

---
+++ src/Components/NewFriend.vue
@@ -0,0 +1,46 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label for="name"> Name</label>
+            <input type="text" name="name" id="name" v-model="enteredName"/>
+        </div>
+        <div>
+            <label for="Phone"> Phone</label>
+            <input type="text" name="Phone" id="Phone" v-model="enteredPhone"/>
+        </div>
+        <div>
+            <label for="Email"> E-mail</label>
+            <input type="email" name="Email" id="Email" v-model="enteredEmail"/>
+        </div>
+
+        <div>
+            <button>Add Contact</button>
+        </div>
+
+    </form>
+</template>
+
+
+<script>
+
+export default {
+
+    emits : ['add-contact'],
+
+    data(){
+        return {
+            enteredName : '',
+            enteredPhone : '',
+            enteredEmail : '',
+        }
+    } ,
+    methods : {
+
+        submitData(){
+            this.$emit('add-contact',this.enteredName ,this.enteredPhone ,this.enteredEmail)
+        }
+    },
+
+}
+
+</script>
