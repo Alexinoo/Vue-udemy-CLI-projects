f6c28479b9c8f90a259f88c517c35ebc38a5f873
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 17:04:42 +0300

    Added a button for toggleFavorite friend - Need to add another data property to work around since props cannot be mutated

--- src/Components/FriendContact.vue
+++ src/Components/FriendContact.vue
@@ -1,7 +1,9 @@
 
 <template>
     <li>
-        <h2> {{ name }}</h2>
+        <h2> {{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : ''}}</h2>
+        <button @click="toggleFavorite">Toggle Favorite</button>
+
         <button @click="toggleDetails"> {{ areDetailsVisible ? 'Hide' : 'Show'}} Details</button>
         <ul v-if="areDetailsVisible">
             <li><strong>Phone : </strong>{{ phoneNumber }}</li>
@@ -19,19 +21,20 @@
 
         - use camelCase for props in the props object
 
-        - But in the template - use kebab-case
+        - But in the template - use kebab-case ( Parent component )
 
         -Props are accessible anywhere in the config object / or even in the template
 
         - We can use this.property to access/set the property values
 
+        -Props cannot be mutated - Vue uses Unidirectional data flow - Their values cannot be changed as forwarded by Parent COmponent
+
+        -But if we would like to change  we have to let the parent know that we would like to change the value of the property
+            -Therefore we need to add a data property that receives the property from the Parent
+
         */
 
-            props : [
-                'name' ,
-                'phoneNumber',
-                'emailAddress'  
-                 ],
+            props : [ 'name' ,     'phoneNumber',  'emailAddress' ,  'isFavorite' ],
 
             data(){
 
@@ -42,14 +45,24 @@
                         name: 'Manuel Lorenz',
                         phone: '+254 4934 6734 783',
                         email: 'manuel@localhost.com'
-                    }
+                    },
+                    friendIsFavorite : this.isFavorite ,
                 }
             },
 
             methods : {
                 toggleDetails(){
                     this.areDetailsVisible = !this.areDetailsVisible
-                }
+                } ,
+
+                toggleFavorite(){
+                    
+                    if(this.friendIsFavorite === '1'){
+                        this.friendIsFavorite = '0'
+                    }else{
+                        this.friendIsFavorite = '1'
+                    }
+                },
             }
 
     }
