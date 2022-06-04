7e7313cfb5ebe7f14eda79857b3cb17edbf294b5
Alex Mwangi <alex@stl-horizon.com>
2022-06-04 14:54:31 +0300

    Creating a Basic Vue app

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+</template>
+
+
+<script>
+/*
+ - We create a standard JavaScript Object
+
+ - We can store it in a variable if we want to
+
+*/
+
+const app = {
+
+    data(){
+
+        return {
+
+            friends : [
+                {
+                id : 'manuel',
+                name : 'Manuel Lorenz',
+                phone : '+254 4934 6734 783',
+                email : 'manuel@localhost.com'
+            },
+                {
+                id : 'julie',
+                name : 'Julie Jones',
+                phone : '+119 934 6734',
+                email : 'julie@localhost.com'
+            },
+            ],
+        }
+    },
+
+    // methods : {} ,
+
+    // computed : {},
+
+}
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+// Main entry point for JS
+
+import { createApp } from 'vue';
+
+createApp({}).mount('');
